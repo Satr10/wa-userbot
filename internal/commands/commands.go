2836c23e1bdc98ab6d6a8971148a4e80c9b5cac0
Satrio <personal.satrio@protonmail.com>
2025-09-10 15:35:33 +0700

    add func to register commands and their permission

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -4,6 +4,16 @@ import (
 	"go.mau.fi/whatsmeow"
 )
 
+func registerCommands() map[string]*Command {
+	commands := make(map[string]*Command)
+
+	commands["ping"] = &Command{
+		PermissionLevel: Everyone,
+	}
+
+	return commands
+}
+
 func PingCommand(c Command) (whatsmeow.SendResponse, error) {
 	return ReplyToTextMesssage(TextMessage{ctx: c.ctx, client: c.client, evt: c.evt, text: "Pong"})
 }
