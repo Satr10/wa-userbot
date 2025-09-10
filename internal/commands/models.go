080c7593496097bbc16e23ea7c72cfa2632bd801
Satrio <personal.satrio@protonmail.com>
2025-09-10 15:37:16 +0700

    add permissionlevel

--- internal/commands/models.go
+++ internal/commands/models.go
@@ -7,9 +7,19 @@ import (
 	"go.mau.fi/whatsmeow/types/events"
 )
 
+type PermissionLevel int
+
+const (
+	Everyone PermissionLevel = iota
+	GroupAdmin
+	SuperAdmin
+	Owner
+)
+
 type Command struct {
 	ctx    context.Context
 	client *whatsmeow.Client
 	evt    *events.Message
 	args   []string
+	PermissionLevel PermissionLevel
 }
